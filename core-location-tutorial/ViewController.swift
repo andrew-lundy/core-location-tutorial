3e6018614629dc6b54705d2e2e8bdbe4b0190215
Andrew Lundy <andrewlundy@icloud.com>
2020-08-16 02:14:44 -0500

    Finish Stage 1 - Set Up

--- core-location-tutorial/ViewController.swift
+++ core-location-tutorial/ViewController.swift
@@ -6,15 +6,51 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var changeLocationBttn: UIButton!
+    @IBOutlet weak var locationDataLbl: UILabel!
+    
+    var locationManager: CLLocationManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        changeLocationBttn.layer.cornerRadius = 10
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
         
     }
-
-
+    
+    @IBAction func changeLocationBttnTapped(_ sender: Any) {
+        if CLLocationManager.locationServicesEnabled() {
+            // Request when in use authorization status.
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
 }
+
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        // Create a switch statement for the CLLocationManager.authorizationStatus() value
+        switch CLLocationManager.authorizationStatus() {
+        
+        // Compare the switch value to the authorizedWhenInUse case
+        case .authorizedWhenInUse:
+            // Start reporting the user's location
+            locationManager.startUpdatingLocation()
+            guard let currentLocation = locationManager.location else { return }
+            
+            locationDataLbl.text = "\(currentLocation.coordinate)"
+            print("LOCATION: \(currentLocation.timestamp)")
+        default:
+            return
+        }
+    }
+}
+
+
+
+
 
